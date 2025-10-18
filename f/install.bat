60a31bfd25d200ec6bb329f6f7e33fcdde5674eb
Bw11111 <91095401+Bw11111@users.noreply.github.com>
2025-10-17 23:49:40 -0700

    Create install.bat

---
+++ f/install.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+set "CurrentDir=%~dp0"
+set "ExePath=%CurrentDir%shitshat.exe"
+echo Creating service...
+sc create WifiMonitorService binPath= "%ExePath%"
+echo Starting service...
+sc start WifiMonitorService
+pause
